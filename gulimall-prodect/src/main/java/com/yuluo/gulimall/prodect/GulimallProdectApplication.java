0de1e577ac14d292615cf994d675b577fce79399
yuluo <yuluo25@gmail.com>
2022-05-02 20:58:52 +0800

    项目初始结构创建

---
+++ gulimall-prodect/src/main/java/com/yuluo/gulimall/prodect/GulimallProdectApplication.java
@@ -0,0 +1,13 @@
+package com.yuluo.gulimall.prodect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProdectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProdectApplication.class, args);
+    }
+
+}
---
+++ gulimall-prodect/src/test/java/com/yuluo/gulimall/prodect/GulimallProdectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuluo.gulimall.prodect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProdectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
